370196ed3f7a599c157ec241cff4ee50fce30c33
bmg1203 <bmg1203@naver.com>
2024-12-07 02:54:03 +0900

    [refactor]: WorkOrder 휴일과 평일 근무자 추가 기능들의 메소드를 분리
    - 메소드들이 한가지 기능만 하도록 분리합니다.

--- src/main/java/oncall/domain/WorkOrder.java
+++ src/main/java/oncall/domain/WorkOrder.java
@@ -14,6 +14,9 @@ public class WorkOrder {
     private final Deque<String> workOrder;
     private int weekIndex = 0;
     private int holidayIndex = 0;
+    private Stack<String> tempWeekSave = new Stack<>();
+    private Stack<String> tempHolidaySave = new Stack<>();
+    private static final Holidays holidays = new Holidays();
 
     public WorkOrder(DateInfo dateInfo, Employees employees) {
         this.workOrder = calculateWorkOrder(dateInfo, employees);
@@ -25,47 +28,51 @@ public class WorkOrder {
 
     private Deque<String> calculateWorkOrder(DateInfo dateInfo, Employees employees) {
         int startDay = dateInfo.getDayOfTheWeek();
-        Holidays holidays = new Holidays();
         Deque<String> workOrder = new ArrayDeque<>();
-        Stack<String> week = new Stack<>();
-        Stack<String> holiday = new Stack<>();
-        for (int i = 1, day = startDay; i <= dateInfo.getLastDay(); i++, day++) {
-            if (day > SUNDAY) {
-                day = day % SUNDAY;
-            }
 
-            //휴일
-            if (day == SATURDAY || day == SUNDAY || holidays.isHoliday(dateInfo.getMonth(), i)) {
-                addHolidayWorker(workOrder, holiday, employees.getHolidayWorker());
-            }
-            //평일
-            if (day != SATURDAY && day != SUNDAY && !holidays.isHoliday(dateInfo.getMonth(), i)) {
-                addWeekWorker(workOrder, week, employees.getWeekWorker());
-            }
+        for (int i = 1, day = startDay; i <= dateInfo.getLastDay(); i++, day++) {
+            if (day > SUNDAY)
+                day = day % SUNDAY;
+
+            if (day == SATURDAY || day == SUNDAY || holidays.isHoliday(dateInfo.getMonth(), i))
+                addHolidayWorker(workOrder, employees.getHolidayWorker());
+
+            if (day != SATURDAY && day != SUNDAY && !holidays.isHoliday(dateInfo.getMonth(), i))
+                addWeekWorker(workOrder, employees.getWeekWorker());
         }
         return workOrder;
     }
 
-    private void addHolidayWorker(Deque<String> workOrder, Stack<String> holiday, List<String> holidayWorker) {
+    private void addHolidayWorker(Deque<String> workOrder, List<String> holidayWorker) {
         String curWorker = holidayWorker.get(holidayIndex);
         if (!workOrder.isEmpty()) {
             String lastName = workOrder.peekLast();
             if (lastName.equals(curWorker)) {
-                holiday.add(curWorker);
-                holidayIndexAdder(holidayWorker);
-                workOrder.add(holidayWorker.get(holidayIndex));
+                holidayDuplicateProcess(workOrder, holidayWorker, curWorker);
                 return;
             }
         }
-
-        if (!holiday.isEmpty()) {
-            holidayIndexAdder(holidayWorker);
-            workOrder.add(holiday.pop());
+        if (!tempHolidaySave.isEmpty()) {
+            holidayStackProcess(workOrder, holidayWorker);
             return;
         }
+        holidayNormalProcess(workOrder, holidayWorker, curWorker);
+    }
 
+    private void holidayNormalProcess(Deque<String> workOrder, List<String> holidayWorker, String curWorker) {
         workOrder.add(curWorker);
         holidayIndexAdder(holidayWorker);
+    }
+
+    private void holidayStackProcess(Deque<String> workOrder, List<String> holidayWorker) {
+        holidayIndexAdder(holidayWorker);
+        workOrder.add(tempHolidaySave.pop());
+    }
+
+    private void holidayDuplicateProcess(Deque<String> workOrder, List<String> holidayWorker, String curWorker) {
+        tempHolidaySave.add(curWorker);
+        holidayIndexAdder(holidayWorker);
+        workOrder.add(holidayWorker.get(holidayIndex));
     }
 
     private void holidayIndexAdder(List<String> holidayWorker) {
@@ -75,26 +82,36 @@ public class WorkOrder {
         }
     }
 
-    private void addWeekWorker(Deque<String> workOrder, Stack<String> week, List<String> weekWorker) {
+    private void addWeekWorker(Deque<String> workOrder, List<String> weekWorker) {
         String curWorker = weekWorker.get(weekIndex);
         if (!workOrder.isEmpty()) {
             String lastName = workOrder.peekLast();
             if (lastName.equals(curWorker)) {
-                week.add(curWorker);
-                weekIndexAdder(weekWorker);
-                workOrder.add(weekWorker.get(holidayIndex));
+                weekDuplicateProcess(workOrder, weekWorker, curWorker);
                 return;
             }
         }
-
-        if (!week.isEmpty()) {
-            weekIndexAdder(weekWorker);
-            workOrder.add(week.pop());
+        if (!tempWeekSave.isEmpty()) {
+            weekStackProcess(workOrder, weekWorker);
             return;
         }
+        weekNormalProcess(workOrder, weekWorker, curWorker);
+    }
 
+    private void weekNormalProcess(Deque<String> workOrder, List<String> weekWorker, String curWorker) {
         workOrder.add(curWorker);
         weekIndexAdder(weekWorker);
+    }
+
+    private void weekStackProcess(Deque<String> workOrder, List<String> weekWorker) {
+        weekIndexAdder(weekWorker);
+        workOrder.add(tempWeekSave.pop());
+    }
+
+    private void weekDuplicateProcess(Deque<String> workOrder, List<String> weekWorker, String curWorker) {
+        tempWeekSave.add(curWorker);
+        weekIndexAdder(weekWorker);
+        workOrder.add(weekWorker.get(holidayIndex));
     }
 
     private void weekIndexAdder(List<String> weekWorker) {
